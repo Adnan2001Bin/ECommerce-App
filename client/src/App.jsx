8fc3d31803ca91b12931e65beeb79e3d0f38336b
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-08 19:30:48 +0600

    make  some dummypages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AuthLayout from './components/ui/auth/layout'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/register'
+import { useState } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/register";
+import AdminLayout from "./components/Admin-View/layout";
+import AdminDashboard from "./pages/Admin-View/Dashboard";
+import AdminFeatures from "./pages/Admin-View/Features";
+import AdminOrder from "./pages/Admin-View/Order";
+import AdminProducts from "./pages/Admin-View/Products";
+import NotFound from "./pages/Not-Found";
+import ShoppingLayout from "./components/Shopping-View/layout";
+import ShoppingAccount from "./pages/Shopping-View/Account";
+import ShoppingCheckout from "./pages/Shopping-View/Checkout";
+import ShoppingHome from "./pages/Shopping-View/Home";
+import ShoppingListing from "./pages/Shopping-View/Listing";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <AuthLayout />,
       children: [
         {
-          path:'/Login',
-          element:<Login />
+          path: "login",
+          element: <Login />,
         },
         {
-          path:'/Register',
-          element:<Register />
-        }
-      ]
-    }
-  ])
+          path: "register",
+          element: <Register />,
+        },
+      ],
+    },
+    {
+      path: "/admin",
+      element: <AdminLayout />,
+      children: [
+        {
+          path: "dashboard",
+          element: <AdminDashboard />,
+        },
+        {
+          path: "features",
+          element: <AdminFeatures />,
+        },
+        {
+          path: "order",
+          element: <AdminOrder />,
+        },
+        {
+          path: "products",
+          element: <AdminProducts />,
+        },
+      ],
+    },
+    {
+      path: "Shop",
+      element: <ShoppingLayout />,
+      children: [
+        {
+          path: "Home",
+          element: <ShoppingHome />,
+        },
+        {
+          path: "Listing",
+          element: <ShoppingListing />,
+        },
+        {
+          path: "Checkout",
+          element: <ShoppingCheckout />,
+        },
+        {
+          path: "Account",
+          element: <ShoppingAccount />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
 
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Admin-View/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AdminHeader() {
+  return (
+    <div>
+      <h1>
+        Header
+      </h1>
+    </div>
+  )
+}
+
+export default AdminHeader
