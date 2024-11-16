56fa0e8c9d55ee9671b0df5bca13f7874fb92619
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-16 22:35:52 +0600

    loginUser in server

--- server/src/controllers/auth/auth.controller.js
+++ server/src/controllers/auth/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import {User} from "../../models/user.model.js";
+import { User } from "../../models/user.model.js";
 //register
 
 const registerUser = async (req, res) => {
@@ -14,7 +14,7 @@ const registerUser = async (req, res) => {
         message: "User Already exists with the same email! Please try again",
       });
 
-    const hashPassword = await bcrypt.hash(password,12);
+    const hashPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
       userName,
       email,
@@ -31,11 +31,66 @@ const registerUser = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Some error occured",
-      error:e
+      error: e,
     });
   }
 };
 
+// login
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-export { registerUser };
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser)
+      return res.json({
+        success: false,
+        message: "User doesn't exists! Please register first",
+      });
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+
+    if (!checkPasswordMatch)
+      return res.json({
+        success: false,
+        message: "Incorrect password! Please try again",
+      });
+
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        role: checkUser.role,
+        email: checkUser.email,
+        userName: checkUser.userName,
+      },
+      "CLIENT_SECRET_KEY",
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      },
+
+    );
+
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+        userName: checkUser.userName,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+      error: e,
+    });
+  }
+};
+
+export { registerUser,loginUser };
