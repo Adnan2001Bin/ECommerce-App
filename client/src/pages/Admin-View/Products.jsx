f754d5b775f5d38fddfd6ea5fe57e91d29d3a5ef
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-17 18:36:43 +0600

    file handleing

--- client/src/pages/Admin-View/Products.jsx
+++ client/src/pages/Admin-View/Products.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import ProductImageUpload from "@/components/Admin-View/image-upload";
+import CommonForm from "@/components/Common/Form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/Config";
+import React, { Fragment, useState } from "react";
+
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+  averageReview: 0,
+};
 
 function AdminProducts() {
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+
+  function onSubmit() {}
   return (
-    <div>
-      <h1>AdminProducts</h1>
-    </div>
-  )
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+        <Sheet
+          open={openCreateProductsDialog}
+          onOpenChange={() => {
+            setOpenCreateProductsDialog(false);
+          }}
+        >
+          <SheetContent side="right" className="overflow-auto">
+            <SheetHeader>
+              <SheetTitle>Add New Product</SheetTitle>
+            </SheetHeader>
+
+            <ProductImageUpload
+              imageFile={imageFile}
+              setImageFile={setImageFile}
+              uploadedImageUrl={uploadedImageUrl}
+              setuploadedImageUrl={setUploadedImageUrl}
+            />
+            <div>
+              <CommonForm
+                onSubmit={onSubmit}
+                formData={formData}
+                setFormData={setFormData}
+                formControls={addProductFormElements}
+                className="py-6"
+              />
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </Fragment>
+  );
 }
 
-export default AdminProducts
+export default AdminProducts;
