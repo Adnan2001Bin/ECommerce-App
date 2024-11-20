3a0a174988cfa7de71a921e7142f1f09791595ad
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-20 18:41:35 +0600

    add routes for add,edit,delete,get

--- server/src/routes/admin/products-routes.js
+++ server/src/routes/admin/products-routes.js
@@ -1,12 +1,20 @@
-import express from "express"
+import express from "express";
 
-import {handleImageUpload} from "../../controllers/admin/products-controller.js"
-import { upload } from "../../helpers/cloudinary.js"
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  fetchAllProducts,
+  handleImageUpload,
+} from "../../controllers/admin/products-controller.js";
+import { upload } from "../../helpers/cloudinary.js";
 
-
-const router = express.Router()
+const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
-
+router.post("/add", addProduct);
+router.put("/edit/:id", editProduct);
+router.delete("/delete/:id", deleteProduct);
+router.get("/get", fetchAllProducts);
 
 export default router;
