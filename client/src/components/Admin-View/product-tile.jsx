03701d1de567a2c9a2d4dc45872b5a6441b4dee2
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-22 17:29:20 +0600

    edit product

--- client/src/components/Admin-View/product-tile.jsx
+++ client/src/components/Admin-View/product-tile.jsx
@@ -2,105 +2,57 @@ import React from "react";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { Button } from "../ui/button";
 
-function AdminProducts() {
-  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
-    useState(false);
-
-  const [formData, setFormData] = useState(initialFormData);
-
-  const [imageFile, setImageFile] = useState(null);
-  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
-  const [imageLoadingState, setImageLoadingState] = useState(false);
-  const [currentEditedId, setCurrentEditedId] = useState(null);
-
-  const { productList } = useSelector((state) => state.adminProducts);
-  const dispatch = useDispatch();
-  const { toast } = useToast();
-
-  function onSubmit(event) {
-    event.preventDefault();
-
-    dispatch(
-      addNewProduct({
-        ...formData,
-        image: uploadedImageUrl,
-      })
-    ).then((data) => {
-      console.log(data);
-
-      if (data?.payload?.success) {
-        dispatch(fetchAllProducts());
-        setOpenCreateProductsDialog(false);
-        setImageFile(null);
-        setFormData(initialFormData);
-        toast({
-          title: "Product add successfully",
-        });
-      }
-    });
-  }
-
-  useEffect(() => {
-    dispatch(fetchAllProducts());
-  }, [dispatch]);
-  console.log(productList, uploadedImageUrl, "productList");
-
+function AdminProductTile({
+  product,
+  setFormData,
+  setOpenCreateProductsDialog,
+  setCurrentEditedId,
+  handleDelete,
+}) {
   return (
-    <Fragment>
-      <div className="mb-5 w-full flex justify-end">
-        <Button onClick={() => setOpenCreateProductsDialog(true)}>
-          Add New Product
-        </Button>
-      </div>
+    <Card className="w-full max-w-sm mx-auto">
+      <div>
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="w-full h-[300px] object-cover rounded-t-lg"
+          />
+        </div>
 
-      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {productList && productList.length > 0
-          ? productList.map((productItem) => (
-              <AdminProductTile
-                setOpenCreateProductsDialog={setOpenCreateProductsDialog}
-                product={productItem}
-                setCurrentEditedId={setCurrentEditedId}
-                setFormData={setFormData}
-              />
-            ))
-          : null}
-      </div>
+        <CardContent>
+          <h2 className="text-xl font-bold mb-2 mt-2">{product?.title}</h2>
 
-      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
-        <Sheet
-          open={openCreateProductsDialog}
-          onOpenChange={() => {
-            setOpenCreateProductsDialog(false);
-          }}
-        >
-          <SheetContent side="right" className="overflow-auto">
-            <SheetHeader>
-              <SheetTitle>Add New Product</SheetTitle>
-            </SheetHeader>
+          <div className="flex justify-between items-center mb-2">
+            <span
+              className={`${
+                product?.salePrice > 0 ? "line-through" : ""
+              } text-lg font-semibold text-primary`}
+            >
+              ${product?.price}
+            </span>
 
-            <ProductImageUpload
-              imageFile={imageFile}
-              setImageFile={setImageFile}
-              uploadedImageUrl={uploadedImageUrl}
-              setUploadedImageUrl={setUploadedImageUrl}
-              setImageLoadingState={setImageLoadingState}
-              imageLoadingState={imageLoadingState}
-              isEditMode = {currentEditedId !== null}
-            />
-            <div className="py-6">
-              <CommonForm
-                onSubmit={onSubmit}
-                formData={formData}
-                setFormData={setFormData}
-                formControls={addProductFormElements}
-                className="py-6"
-              />
-            </div>
-          </SheetContent>
-        </Sheet>
+            {product?.salePrice > 0 ? (
+              <span className="text-lg font-bold">${product?.salePrice}</span>
+            ) : null}
+          </div>
+        </CardContent>
+
+        <CardFooter className="flex justify-between items-center">
+          <Button
+            onClick={() => {
+              setOpenCreateProductsDialog(true);
+              setCurrentEditedId(product?._id);
+              setFormData(product);
+            }}
+          >
+            Edit
+          </Button>
+          <Button onClick={() => handleDelete(product?._id)}>Delete</Button>
+        </CardFooter>
       </div>
-    </Fragment>
+    </Card>
   );
 }
 
-export default AdminProducts;
+export default AdminProductTile;
